b4545abb29bc008ac92a353799a7fe032480ead3
hsisco <hysensisco@gmail.com>
2020-04-30 22:08:13 -0400

    enhancer

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,28 @@
 const enhancer = require('./enhancer.js');
+
+const { repair, succeed, fail } = require('./enhancer.js');
+
+const item = {
+  name: 'Burt',
+  enhancement: 13, // number from 0 to 20
+  durability: 76   // number from 0 to 100
+}
 // test away!
+
+describe('repair', () => {
+  it('', () => {
+
+  })
+});
+
+describe('succeed', () => {
+  it('', () => {
+
+  })
+});
+
+describe('fail', () => {
+  it('', () => {
+
+  })
+});
